e3ee4ce7d6f0e1677187bbc584098ba409619c92
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-04-13 16:10:22 +0200

    add carouse, add room tiles, add youtube from api, add alternating tiles - done externally

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        YT: true, // Add YT as a global variable
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error"],
+    },
+  },
   pluginJs.configs.recommended,
 ];
